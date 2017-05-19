a7de712f45327006c7851f10d81d941fff9b692e
silveirago <gustavosilveira@MacBook-Pro-4.local>
2017-05-19 19:07:57 -0400

    Arquivos adicionados

---
+++ batedor_de_panela_automatico/batedor_de_panela_automatico.ino
@@ -0,0 +1,36 @@
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val = 15;    // variable to read the value from the analog pin 
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  
+
+}
+
+void loop() {
+
+  int reading = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = map(reading, 0, 1023, 1000, 100);     // scale it to use it with the servo (value between 0 and 180) 
+  
+//   for (pos = 0; pos <= 45; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(val);                       // waits 15ms for the servo to reach the position
+//  
+//   }
+if (reading > 60) {
+   myservo.write(45);              // tell servo to go to position in variable 'pos'
+    delay(val);
+    myservo.write(0);              // tell servo to go to position in variable 'pos'
+ delay(100);
+ Serial.println(reading);
+}
+
+
+}
